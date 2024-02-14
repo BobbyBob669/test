3e98eefe32f5840e8d100432c072164efe57a2dc
BobbyBob <pssttt1@gmail.com>
2024-02-14 14:01:20 +0700

    tugas flutter 2

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/pages/home/home_page.dart';
+import 'package:weather_app/services/local_storage.dart';
+import 'package:weather_app/services/service_locator.dart';
+import 'package:provider/provider.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  setupServiceLocator();
+  await getIt<LocalStorage>().init();
+  runApp(const WeatherApp());
+}
+
+class WeatherApp extends StatelessWidget {
+  const WeatherApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ProviderScope(
+        child: MyHomePage(),
+      ),
+    );
+  }
+}
